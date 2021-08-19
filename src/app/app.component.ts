6c81467916758c46fd1dd3100802ee921946fcfc
梁帅帅 <Super_Mumoo@163.com>
2021-08-19 18:32:18 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'workCostPerformance';
+  /** 平均日薪 6 */
+  dailyWage = 500;
+  /** 工作时长 7 */
+  workingHours = 8;
+  /** 通勤时长 8 */
+  commuteHours = 1;
+  /** 摸鱼时长 9 */
+  loafHours = 2;
+  /** 学历 10 */
+  education = 1;
+  /** 工作环境 11 */
+  workEnvironment = 1;
+  /** 异性环境 12 */
+  oppositeSixEnvironment = 1;
+  /** 同事环境 13 */
+  colleagueEnvironment = 1;
+  /** 职业资格 14 */
+  qualificationsEnvironment = 1;
+  // 是/** 30前上班 15 */
+  load830 = 1;
+  /** 城市环境 16 */
+  cityEnvironment = 1;
+  // ((D6*D11*D12*D13)/(35*(D7+D8-0.5*D9)*D10*D14))*D15
+  // ((D6*D11*D12*D13/D16)/(35*(D7+D8-0.5*D9)*D10*D14))*D15
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    NzSelectModule,
+    NzInputNumberModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
